4621cf1ad14ffbef327b46c6357b841ef028312d
rafaelandrade74 <rafael_andrade74@hotmail.com>
2016-08-22 20:27:27 -0300

    Add files via upload

---
+++ 1037.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+double entrada;
+
+scanf("%lf",&entrada);
+
+if (entrada >= 0 && entrada <= 25){
+	printf("Intervalo (0,25]\n");
+}
+else if(entrada > 25 && entrada <= 50){
+	printf("Intervalo (25,50]\n");
+}
+else if (entrada > 50 && entrada <= 75){
+	printf("Intervalo (50,75]\n");
+}
+else if (entrada > 75 && entrada <= 100){
+	printf("Intervalo (75,100]\n");
+}
+else {
+	printf("Fora de intervalo\n");
+}
+
+	return 0;
+}
